465c1bb55fce31fd3da16d64d8b36036eba86932
Mayuresh Dhakad <106756562+mayureshdhakad@users.noreply.github.com>
2023-04-15 21:18:56 +0530

    Delete Day1_Print&Replit_Workspace directory

--- Day1_Print&Replit_Workspace/main.py
+++
@@ -1,5 +0,0 @@
-print("Mark")
-print("September 20th 2029")
-print("I am signing up for Replit's 100 days of Python challnege!")
-print("I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.")
-print("I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happen to be. No excuses for not coding from the middle of a field!")
