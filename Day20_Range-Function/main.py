14004261d21fb25dafbab3d4df07e415044e5a9f
mayurrd <dhakadmayuresh@gmail.com>
2023-04-18 18:01:38 +0530

    done

---
+++ Day20_Range-Function/main.py
@@ -0,0 +1,7 @@
+print("Challenge 20")
+x= int(input("Enter a starting limit: "))
+y= int(input("Enter a Ending limit: "))
+z= int(input("Enter the increment: "))
+
+for i in range (x,y,z):
+  print(i)
